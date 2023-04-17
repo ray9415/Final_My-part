60d87445bd690c6481754652ba14e1b0a40b1693
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-04-17 19:05:47 +0800

    .

---
+++ final_repo_test/final_repo_test/Areas/LoginStatus/Controllers/LoginStatus_HomeController.cs
@@ -0,0 +1,24 @@
+﻿using final_repo_test.Data;
+using final_repo_test.Interfaces;
+using final_repo_test.ViewModels.LoginStatus;
+using Microsoft.AspNetCore.Mvc;
+
+namespace final_repo_test.Areas.LoginStatus.Controllers
+{
+    [Area("LoginStatus")]
+
+
+    public class LoginStatus_HomeController : Controller
+    {
+        private readonly ILoginStatusRepository _loginStatusRepository;
+        public LoginStatus_HomeController(ILoginStatusRepository loginStatusRepository)
+        {
+            _loginStatusRepository = loginStatusRepository;
+        }
+        public async Task<IActionResult> Index(string Chart = "Day")
+        {
+            GetDayViewModel data = await _loginStatusRepository.GetDay(Chart);
+            return View(data);
+        }
+    }
+}
---
+++ final_repo_test/final_repo_test/ViewModels/LoginStatus/GetDayViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace final_repo_test.ViewModels.LoginStatus
+{
+    public class GetDayViewModel
+    {
+        public List<DateTime> Mainx { get; set; } = new List<DateTime>();
+        public List<int> Mainy { get; set; } = new List<int>();
+        public List<TimeSpan> Headx { get; set; } = new List<TimeSpan>() { TimeSpan.Zero,new TimeSpan(6,0,0), new TimeSpan(12, 0, 0), new TimeSpan(18, 0, 0) };
+        public List<int> Heady { get; set; } = new List<int>();
+        public int Average { get; set; } = default!;
+    }
+}
